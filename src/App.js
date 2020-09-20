c4fc0a865a6dc97ff180bbe5248b7ee7571a8445
Anna <annaszczekdev@gmail.com>
2020-09-20 22:11:19 +0200

    add link to github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather Search Engine</h1>
-      <Search />
-    </div>
+      <footer />
+     This project is created
+       
+          <a href="https://github.com/anna262/my-app" target="“blank”
+            >Open-source code</a
+          >
+          by Anna Szczek
+      <footer />
+    <div/>
   );
-}
+
