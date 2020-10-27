4e07529dcaaa7d5dbc08f27039b2b6efc7d5e028
anna262 <67240544+anna262@users.noreply.github.com>
2020-10-27 16:50:00 +0100

    Create Weather.js

---
+++ src/Weather.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+import WeatherInfo from "./WeatherInfo";
+import "./Weather.css";
+import axios from "axios";
+import WeatherForecast from "./WeatherForecast";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name
+    });
+  }
+
+  function search() {
+    const apiKey = "35e83019a6881c6f862e82a156e4c5fc";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(city);
+    //search for a city
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Look for a city"
+            className="form-control"
+            autoFocus="on"
+            onChange={handleCityChange}
+          />
+          <input type="submit" value="search" />
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast city={weatherData.city} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading";
+  }
+}
