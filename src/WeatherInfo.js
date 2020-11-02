820400929921f4aabf50819d4cc18f191a9ae9cb
Anna <annaszczekdev@gmail.com>
2020-11-02 12:07:10 +0100

    updated

---
+++ src/WeatherInfo.js
@@ -0,0 +1,35 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="weatherInfo">
+      <h1> {props.data.city} </h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-9">
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+          </div>
+          <div className="float-left">
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
+        </div>
+
+        <div className="col-9">
+          <ul>
+            <li>Humidity {props.data.humidity}% </li>
+            <li>Wind {props.data.wind}km/h</li>{" "}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
